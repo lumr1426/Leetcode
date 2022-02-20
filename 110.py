5b30b770d80d5ed900e1c32866170ceecbacb2f1
LUMR <75212422+lumr1426@users.noreply.github.com>
2022-02-20 15:15:47 +0900

    Create 110.py

---
+++ 110.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def height(self, root: Optional[TreeNode]) -> int:
+        if root==None: return -1
+        else: return max(self.height(root.left),self.height(root.right))+1
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root==None: return True
+        left=self.height(root.left)
+        right=self.height(root.right)
+        if(abs(left-right)<=1): return self.isBalanced(root.left) and self.isBalanced(root.right)
+        else: return False
+        
