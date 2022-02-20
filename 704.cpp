d5d4c90bca092c9cbf110727dbc8cf4475e0887a
Daniel Cha <daniel1426551@gmail.com>
2022-02-20 16:44:22 +0900

    Create 704.cpp

---
+++ 704.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int pivot, left=0, right=nums.size()-1;
+        while(left<=right)
+        {
+            pivot=left+(right-left)/2;
+            if(nums[pivot]==target)
+                return pivot;
+            else if(nums[pivot]>target)
+                right=pivot-1;
+            else
+                left=pivot+1;
+        }
+        return -1;
+    }
+};
