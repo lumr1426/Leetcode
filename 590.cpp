ad654f84817253cb20b1873745505e5580b634b5
LUMR <75212422+lumr1426@users.noreply.github.com>
2022-02-20 15:11:57 +0900

    Create 590.cpp

---
+++ 590.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    
+public:
+    vector<int> postorder(Node* root) {
+        if(root==NULL) return {};
+        vector<int> result;
+        stack<Node*> s;
+        s.push(root);
+        while(!s.empty())
+        {
+            Node*temp=s.top();
+            s.pop();
+            if(!temp->children.empty())
+            {
+                for(auto child:temp->children)
+                {
+                    s.push(child);
+                }
+            }
+            result.push_back(temp->val);
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
