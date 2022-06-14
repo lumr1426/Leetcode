9b33e33cc8d4285e40f70a7cdc751b9aad4557c2
Daniel Cha <daniel1426551@gmail.com>
2022-06-14 15:49:49 +0900

    Update 3_Longest_Substring_Without_Reapeating_Characters.cpp

--- 3_Longest_Substring_Without_Reapeating_Characters.cpp
+++ 3_Longest_Substring_Without_Reapeating_Characters.cpp
@@ -105,3 +105,20 @@ public:
   #Case 2): just increase the window
 #5. Goes through loop only once -> TC: O(n)
 #6. Store only characters in s -> SC: O(n)
+
+###Python3
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen={}
+        l=0
+        output=0
+        for r in range(len(s)):
+            if s[r] not in seen:
+                output=max(output, r-l+1)
+            else:
+                if seen[s[r]]<l:
+                    output=max(output, r-l+1)
+                else:
+                    l=seen[s[r]]+1
+            seen[s[r]]=r
+        return output
